72a6516c32edf788899295f0bc7a403845d9494c
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-12-28 17:29:32 -0200

    created script to work with selecting text

--- src/select-text.js
+++ src/select-text.js
@@ -2,8 +2,26 @@
 
   'use strict';
 
-  global.SelectText = function(element, callback) {
+  var selection = doc.selection;
+  var getSelection = doc.getSelection;
+  var hasSupport = getSelection || selection;
 
+  var selectText = function(element, callback) {
+    var onMouseUp = function(e) {
+      e.preventDefault();
+
+      var text = getSelection ? getSelection() :
+                 selection.createRange().text;
+      
+      callback(text);
+    };
+
+    element.addEventListener('mouseup', onMouseUp);
+  };
+
+  global.SelectText = function(element, callback) {
+    if (!hasSupport) return;
+    selectText(element, callback);
   };
   
 }(window, document));
