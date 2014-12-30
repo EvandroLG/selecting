b6f9a5cb52aa7ae7d73f2d2f261eb1c23b60d838
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-12-30 11:21:42 -0200

    created test to check if on method is called

--- test/spec.js
+++ test/spec.js
@@ -17,6 +17,20 @@ describe('SelectingText', function(){
   describe('action', function() {
     var element = mockElement();
 
+    var verifyLibrary = function(lib, end) {
+      window[lib] = function(value) {
+        return this;
+      };
+
+      window[lib].prototype = {
+        on: function() {
+          end();
+        }
+      };
+
+      window.SelectingText(new window[lib]('element'), function() {});
+    };
+
     it('should executes function that was passed by parameter after mouseup event was executed', function(end) {
       var method = function() {
         end();
@@ -24,6 +38,14 @@ describe('SelectingText', function(){
 
       window.SelectingText(element, method);
       element.dispatchEvent(new Event('mouseup'));
+    });
+
+    it('should executed on function when jQuery object is passed by parameter', function(end) {
+      verifyLibrary('jQuery', end)
+    });
+
+    it('should executed on function when Zepto object is passed by parameter', function(end) {
+      verifyLibrary('Zepto', end)
     });
 
   });
